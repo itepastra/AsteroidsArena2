f59570b104c8a9b065049509c4523b7ae3db5e51
noa <itepastra@gmail.com>
2022-10-24 11:27:55 +0200

    made an extra datatype for a level to help with IO later

--- src/Model.hs
+++ src/Model.hs
@@ -27,8 +27,6 @@ data InfoToShow
 nO_SECS_BETWEEN_CYCLES :: Float
 nO_SECS_BETWEEN_CYCLES = 1
 
-data Level = Level {name :: String, startState :: GameState}
-
 data GameState
   = GameState
       { elapsedTime :: Float,
@@ -46,30 +44,45 @@ data GameState
   | DeathState {score :: Int}
   | MenuState {levels :: [Level]}
 
+data Level = Level
+  { name :: String,
+    initState :: GameStateInit
+  }
+
+data GameStateInit = GameStateInit
+  { initPlayer :: Player,
+    initAsteroids :: [Asteroid],
+    initWalls :: [Wall]
+  }
+
 newPlayer :: Player
 newPlayer = Player (PhysObj (Point 0 0) (Point 0 0) playerRadius) 100 (Point 0 1) 0
 
 gameStateFromLevel :: StdGen -> [[Gloss.Point]] -> Level -> GameState
-gameStateFromLevel r pts (Level {startState = gs}) = gs {rand = r, starPositions = pts}
+gameStateFromLevel r pts (Level {initState = initState}) =
+  GameState
+    { rand = r,
+      starPositions = pts,
+      elapsedTime = 0,
+      player = initPlayer initState,
+      asteroids = initAsteroids initState,
+      bullets = [],
+      timeSinceLastShot = 10,
+      timeTillNextAsteroid = 0,
+      score = 0,
+      walls = initWalls initState,
+      keys = empty
+    }
 
 defaultLevels :: [Level]
 defaultLevels =
   [ Level
       { name = "empty",
-        startState =
-          GameState
-            { elapsedTime = 0,
-              player = newPlayer,
-              asteroids = [],
-              bullets = [],
-              -- walls = [],
-              walls = [Wall (Point 0 400) (Point 0 (-1)) 450 180, Wall (Point (-400) 0) (Point 1 0) 450 (-90), Wall (Point 0 (-400)) (Point 0 1) 450 0, Wall (Point 400 0) (Point (-1) 0) 450 90],
-              keys = empty,
-              rand = mkStdGen 0,
-              starPositions = [[]],
-              timeSinceLastShot = 0,
-              timeTillNextAsteroid = 0,
-              score = 0
+        initState =
+          GameStateInit
+            { initPlayer = newPlayer,
+              initAsteroids = [],
+              initWalls = [Wall (Point 0 400) (Point 0 (-1)) 450 180, Wall (Point (-400) 0) (Point 1 0) 450 (-90), Wall (Point 0 (-400)) (Point 0 1) 450 0, Wall (Point 400 0) (Point (-1) 0) 450 90]
             }
       }
   ]
