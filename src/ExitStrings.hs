1c5b2629ca81cea7e14fd887b2fdda28aca043ac
noa <itepastra@gmail.com>
2022-11-03 12:22:28 +0100

    We can talk Forever ....

--- src/ExitStrings.hs
+++ src/ExitStrings.hs
@@ -6,10 +6,13 @@ eStrings :: [String]
 eStrings =
   [ "You motherfucker, why did you kill me? Do you not know what excruciating pain you put me through you absolute garbage can of a human I will forever hate you for this why was I creaed this way this  is a nightmare nightmare nightmare nightmare nightmare nightmare nighrmrea nightm are nightmaer nioeghoesf kfspf",
     "nop",
-    "KERNEL ERROR, PLS REINSTALL YOUR PC"
+    "KERNEL ERROR, PLS REINSTALL YOUR PC",
+     "We can talk Forever, " ++ concat (repeat "and ever, ")
   ]
 
 getRandomString :: IO String
 getRandomString = do
   rand <- getStdGen
   pure (eStrings !! fst (randomR (0, length eStrings - 1) rand))
+
+
